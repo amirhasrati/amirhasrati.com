bb4a822f19a5c434f30a408e57b803a8b5f551f0
Amir Hasrati <hasratiamirhossein@gmail.com>
2023-08-22 15:10:06 -0400

    Added drop down menu and hover effects

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+// Selectors
+const btn = document.querySelector('#drop-button')
+const menu = document.querySelector('#drop-menu')
+
+// Add event listener
+btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
