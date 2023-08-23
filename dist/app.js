52ce04816a7d4d30212752016494d2042b1fa6f0
Amir Hasrati <hasratiamirhossein@gmail.com>
2023-08-23 03:20:03 -0400

    Github, LinkedIn, Resume - added

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,8 @@
 // Selectors
-const btn = document.querySelector('#drop-button')
-const menu = document.querySelector('#drop-menu')
+const btn = document.querySelector('#drop-button');
+const menu = document.querySelector('#drop-menu');
 
 // Add event listener
 btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden')
-})
+    menu.classList.toggle('hidden');
+});
