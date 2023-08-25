1a608ae349be67cc712506c3f2c58a6e1821e266
Amir Hasrati <hasratiamirhossein@gmail.com>
2023-08-25 00:12:22 -0400

    Changed home screen description...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./dist/*.{html,js}', './*.{html}'],
+    content: ['./dist/*.{html,js}', './*.html'],
     theme: {
         extend: {},
     },
