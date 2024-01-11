ed01c21caf5fd70afe3ecd47584a9b763d9322ae
Amir Hasrati <amirhasrati@Amirs-MacBook-Pro.local>
2024-01-11 00:01:53 -0500

    Contact Modal Added

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,21 @@
-// Selectors
-const btn = document.querySelector('#drop-button');
-const menu = document.querySelector('#drop-menu');
+const modalBtn = document.querySelector('#open-contact');
+const modal = document.querySelector('#contact-modal');
+const pageContent = document.querySelector('#page-content');
+const navbar = document.querySelector('#navbar');
 
-// Add event listener
-btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-});
+modalBtn.addEventListener('click', e => {
+    modal.classList.remove("hidden");
+    pageContent.classList.add("blur-lg");
+    navbar.classList.add("bg-gray-800")
+    navbar.classList.add("text-gray-200")
+    e.stopPropagation();
+})
+
+window.addEventListener('click', e => {
+    if (!modal.contains(e.target) && e.target !== modal) {
+        modal.classList.add("hidden");
+        pageContent.classList.remove("blur-lg");
+        navbar.classList.remove("bg-gray-800")
+        navbar.classList.remove("text-gray-200")
+    }
+})
