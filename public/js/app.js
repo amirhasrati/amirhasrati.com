d835e54dd4f010bfb07cd498abb0c3cc6a71f333
Amir Hasrati <hasratiamirhossein@gmail.com>
2024-01-21 22:48:38 -0500

    Colour changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,59 +1,57 @@
-const home = document.querySelector('#home');
-const roadmap = document.querySelector('#roadmap');
-const projects = document.querySelector('#projects')
-const about = document.querySelector('#about');
-const contact = document.querySelector('#contact')
+const home = document.querySelector("#home");
+const roadmap = document.querySelector("#roadmap");
+const projects = document.querySelector("#projects");
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
 
-const navbarHome = document.querySelector('#home #navbar');
-const navbarRoadmap = document.querySelector('#roadmap #navbar');
-const navbarProjects = document.querySelector('#projects #navbar')
-const navbarAbout = document.querySelector('#about #navbar');
-const navbarContact = document.querySelector('#contact #navbar')
+const navbarHome = document.querySelector("#home #navbar");
+const navbarRoadmap = document.querySelector("#roadmap #navbar");
+const navbarProjects = document.querySelector("#projects #navbar");
+const navbarAbout = document.querySelector("#about #navbar");
+const navbarContact = document.querySelector("#contact #navbar");
 
-const menuBtn = document.querySelector('#mobile-menu');
-const dropdownOptions = document.querySelector('#dropdown-options');
-const menuOpen = document.querySelector('#btn-open');
-const menuClose = document.querySelector('#btn-close');
+const menuBtn = document.querySelector("#mobile-menu");
+const dropdownOptions = document.querySelector("#dropdown-options");
+const menuOpen = document.querySelector("#btn-open");
+const menuClose = document.querySelector("#btn-close");
 
-menuBtn.addEventListener('click', () => {
-    if(dropdownOptions.classList.contains('hidden')) {
-        dropdownOptions.classList.remove('hidden');
-        menuOpen.classList.add('hidden');
-        menuClose.classList.remove('hidden');
-    } else {
-        dropdownOptions.classList.add('hidden');
-        menuOpen.classList.remove('hidden');
-        menuClose.classList.add('hidden');
-    }
-})
+menuBtn.addEventListener("click", () => {
+  if (dropdownOptions.classList.contains("hidden")) {
+    dropdownOptions.classList.remove("hidden");
+    menuOpen.classList.add("hidden");
+    menuClose.classList.remove("hidden");
+  } else {
+    dropdownOptions.classList.add("hidden");
+    menuOpen.classList.remove("hidden");
+    menuClose.classList.add("hidden");
+  }
+});
 
 // Current page:
 
-
-
-if(window.location.href.includes('home')) {
-    const currentBtn = navbarHome.querySelector('#buttons').children[0];
-    currentBtn.className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-
-} else if(window.location.href.includes('projects')) {
-    const currentBtn = navbarProjects.querySelector('#buttons').children[1];
-    currentBtn.className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-
-} else if(window.location.href.includes('roadmap')) {
-    const currentBtn = navbarRoadmap.querySelector('#buttons').children[2];
-    currentBtn.className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-
-} else if(window.location.href.includes('about')) {
-    const currentBtn = navbarAbout.querySelector('#buttons').children[4];
-    currentBtn.className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-
-} else if(window.location.href.includes('contact')) {
-    const currentBtn = navbarContact.querySelector('#buttons').children[5];
-    currentBtn.className = "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium";
-} 
+if (window.location.href.includes("home")) {
+  const currentBtn = navbarHome.querySelector("#buttons").children[0];
+  currentBtn.className =
+    "bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium";
+} else if (window.location.href.includes("projects")) {
+  const currentBtn = navbarProjects.querySelector("#buttons").children[1];
+  currentBtn.className =
+    "bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium";
+} else if (window.location.href.includes("roadmap")) {
+  const currentBtn = navbarRoadmap.querySelector("#buttons").children[2];
+  currentBtn.className =
+    "bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium";
+} else if (window.location.href.includes("about")) {
+  const currentBtn = navbarAbout.querySelector("#buttons").children[4];
+  currentBtn.className =
+    "bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium";
+} else if (window.location.href.includes("contact")) {
+  const currentBtn = navbarContact.querySelector("#buttons").children[5];
+  currentBtn.className =
+    "bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium";
+}
 
 // mobile menu
 // if(window.location.href.includes('home')) {
 //     navbarHome.className = "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium";
 // }
-
